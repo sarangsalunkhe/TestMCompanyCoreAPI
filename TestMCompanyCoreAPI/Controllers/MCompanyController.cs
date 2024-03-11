2fa4cd755cc38505c59eca52a8dea9c16f89c401
sarangsalunkhe <sarangsalunkhe2@gmail.com>
2024-03-11 19:18:57 +0530

    configer dbcontext and connection string in program.cs file
    add API MCompanyController
    
    create constructor in MCompanyController

---
+++ TestMCompanyCoreAPI/Controllers/MCompanyController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestMCompanyCoreAPI.Models;
+
+namespace TestMCompanyCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MCompanyController : ControllerBase
+    {
+        private readonly DbBrokerageNewContext _context;
+
+        public MCompanyController(DbBrokerageNewContext _context)
+        {
+            this._context = _context;
+        }
+    }
+}
--- TestMCompanyCoreAPI/Program.cs
+++ TestMCompanyCoreAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TestMCompanyCoreAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var provider=builder.Services.BuildServiceProvider();
+var config=provider.GetRequiredService<IConfiguration>();
+builder.Services.AddDbContext<DbBrokerageNewContext>(x=>
+x.UseSqlServer(config.GetConnectionString("Constr")));
 
 var app = builder.Build();
 
