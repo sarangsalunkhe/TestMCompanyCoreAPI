604422a4573b8a52fbbd282dd1a8d1011d47446b
sarangsalunkhe <sarangsalunkhe2@gmail.com>
2024-04-09 11:02:01 +0530

    with implementing add method mcustomer controller is ready

--- TestMCompanyCoreAPI/Controllers/McustomerController.cs
+++ TestMCompanyCoreAPI/Controllers/McustomerController.cs
@@ -34,5 +34,38 @@ namespace TestMCompanyCoreAPI.Controllers
             }
             return Ok(data);
         }
+
+        [HttpPost]
+        public async Task<ActionResult<Mcustomer>>SaveData(Mcustomer model)
+        {
+            await _context.Mcustomers.AddAsync(model);
+            await _context.SaveChangesAsync();
+            return Ok(model);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Mcustomer>>UpdateData(int id,Mcustomer model)
+        {
+            if(id!=model.CustomerId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(model).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return Ok(model);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Mcompany>>DeleteData(int id)
+        {
+            var data = await _context.Mcompanies.FirstOrDefaultAsync(m => m.CompanyId == id);
+            if(data==null)
+            {
+                return NotFound();
+            }
+            _context.Mcompanies.Remove(data);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
